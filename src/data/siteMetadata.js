229257437a5828f0cf7d0f5a0bad37edd71cc525
James <james.bohrman@fermyon.com>
2023-07-14 19:47:16 -0400

    meta

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -2,8 +2,8 @@ export default {
 	name: "Uppercut - The best solution for tech partnerships and third-party integrations",
 	description: "Uppercut is the only integration developer that guarantees savings on time to market and costs or your money back. Tackle roadmaps at lightning speed ⚡️ while leveraging OSS tools without relying on a flaky SaaS platform",
 	keywords: "integrations, tech partnerships, third-party integrations, integration developer, integration development, integrations agency, saas, startups, tech, software, software development, tech partnership agency",
-	author: "James Bohrman",
+	author: "Uppercut",
 	siteUrl: "https://www.uppercut.io",
-	GitHubUrl: "https://github.com/uppercut-ip",
+	GitHubUrl: "https://github.com/uppercut-io",
 	language: "English"
 };
