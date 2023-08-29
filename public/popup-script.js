28e4b202395a3b2037725ed32a22b3b7f544ff07
James <james.bohrman@fermyon.com>
2023-08-29 18:59:42 -0400

    popup

---
+++ public/popup-script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the user has visited before
+    if (!localStorage.getItem("popupDisplayed")) {
+        // Display the pop-up on the first scroll
+        window.addEventListener("scroll", showPopup);
+    }
+
+    function showPopup() {
+        // Show the pop-up
+        const popup = document.querySelector(".popup");
+        popup.style.display = "block";
+
+        // Remove the scroll event listener after displaying the pop-up
+        window.removeEventListener("scroll", showPopup);
+
+        // Set a flag in localStorage to indicate that the pop-up has been displayed
+        localStorage.setItem("popupDisplayed", "true");
+
+        // Set a timer to reset the flag after two weeks
+        setTimeout(function() {
+            localStorage.removeItem("popupDisplayed");
+        }, 2 * 7 * 24 * 60 * 60 * 1000); // Two weeks in milliseconds
+    }
+});
